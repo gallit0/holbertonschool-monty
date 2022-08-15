1486b1cc275c92ba1238a2eacd993bcfe29bd22c
gallit0 <mateogesede@gmail.com>
2022-08-15 13:00:53 -0700

    segmentation fault in doubly linked list:

--- main.c
+++ main.c
@@ -1,38 +1,50 @@
 #include "monty.h"
 
-int execution(char *cont, int i)
+int execution(char *cont, unsigned int i, stack_t **head)
 {
 	char *current;
 	char *word;
-	int j;
+	int j, n;
 	instruction_t inst[] = {
 		{"push", op_push},
 		{"pall", op_pall},
 		{NULL, NULL},
 	};
+
 	if (!cont)
 		return (0);
 	current = strdup(cont);
-	word = strtok(word, " \t$");
+	word = strtok(current, " \t$");
 	for (j = 0; j < 2; j++)
 	{
 		if (strcmp(inst[j].opcode, word) == 0)
 		{
-			printf("found\n");	
+			word = strtok(NULL, " \t$");
+			printf("befpre isdigit: %s\n", word);
+			/*if (isdigit(word) == 0)
+			{
+				dprintf(STDERR_FILENO, "L%u: usage: push integer\n", i);
+				exit(1);
+			}*/
+			printf("before atoi\n");
+			n = atoi(word);
+			printf("%d\n", n);
+			inst[j].f(head , n);
 			return (0);
 		}
 	}
 
 	dprintf(STDERR_FILENO, "L%d: unknown instruction %s", i, word);
-	return (1);
+	exit(1);
 }
 
 int main(int ac, char **av)
 {
 	FILE *o;
-	int i = 0;
+	unsigned int i = 0;
 	char *cont = 0;
 	size_t len = 0;
+	stack_t **head;
 
 	if (ac != 2)
 	{
@@ -49,7 +61,7 @@ int main(int ac, char **av)
 	{
 		cont = strtok(cont, "\n");
 		i++;
-		if (1 == execution(cont, i))
+		if (1 == execution(cont, i, head))
 			return (1);
 	}
 	fclose(o);
--- op.c
+++ op.c
@@ -3,9 +3,34 @@
 
 void op_push(stack_t **stack, unsigned int line_number)
 {
-	
+	stack_t *new, *temp;
+
+	new = malloc(sizeof(stack_t));
+	if (!new)
+	{
+		dprintf(STDERR_FILENO, "Error: malloc failed\n");
+		exit(1);
+	}
+	new->n = (int)line_number;
+	new->next = (*stack);
+	new->prev = NULL;
+	(*stack) = new;
+
+	if (new->next != NULL)
+	{
+		temp = new;
+		new = new->next;
+		new->prev = temp;
+	}
 }
 void op_pall(stack_t **stack, unsigned int line_number)
 {
+	stack_t *p = (*stack);
 
+	(void)line_number;
+	while (p)
+	{
+		printf("%d\n", p->n);
+		p = p->next;
+	}
 }
